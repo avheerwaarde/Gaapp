c79cc6c96f48db46a44be81ceca73792f4e2205e
avheerwaarde <andre.van.heerwaarde@eclectic.eu>
2014-08-17 14:16:43 +0200

    added initial version angular ASN.NET MVC website

---
+++ WebApplication/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/GaappAngularApp")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .IncludeDirectory("~/Scripts/Factories", "*.js")
+                .Include("~/Scripts/GaappAngularApp.js"));
+
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ WebApplication/Scripts/GaappAngularApp.js
@@ -0,0 +1,38 @@
+﻿var GaappAngularApp = angular.module('GaappAngularApp', ['ngRoute', 'ui.bootstrap']);
+
+GaappAngularApp.controller('LandingPageController', LandingPageController);
+GaappAngularApp.controller('LoginController', LoginController);
+GaappAngularApp.controller('RegisterController', RegisterController);
+
+GaappAngularApp.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
+GaappAngularApp.factory('LoginFactory', LoginFactory);
+GaappAngularApp.factory('RegistrationFactory', RegistrationFactory);
+
+var configFunction = function ($routeProvider, $httpProvider, $locationProvider) {
+
+    $locationProvider.hashPrefix('!').html5Mode(true);
+
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo/:donuts', {
+            templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        })
+        .when('/login?returnUrl', {
+            templateUrl: 'Account/Login',
+            controller: LoginController
+        })
+        .when('/register', {
+            templateUrl: '/Account/Register',
+            controller: RegisterController
+        });
+
+    $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+}
+configFunction.$inject = ['$routeProvider', '$httpProvider', '$locationProvider'];
+
+GaappAngularApp.config(configFunction);
